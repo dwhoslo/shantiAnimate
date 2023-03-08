2ab7c79c7ae2b47435ffcb22ea80c143673c3e17
oslo <468133459@qq.com>
2023-03-08 16:44:40 +0800

    emit

--- index.js
+++ index.js
@@ -2,14 +2,31 @@ let app = new Vue({
     el: '#app',
     data(){
         return{
-
+            ShowMessageInfo:'',
+            MessageJsonData:{
+                '嵩山_bgis':{
+                    info:'hrfdt4353edfer643534'
+                },
+                '泰山_bgis':{
+                    info:'gdt435fdst43535werfwet'
+                },
+                '衡山_bgis':{
+                    info:'4242342523rewr43534523'
+                },
+                '恒山_bgis':{
+                    info:'1231254235312321432412'
+                },
+            }
         }
     },
     created() {
-        console.log('Vue is ready!')
+        window.SwitchInfo = this.SwitchInfo
+        this.ShowMessageInfo = this.MessageJsonData.songshan.info
     },
     methods: {
-        
+        SwitchInfo(e){
+            this.ShowMessageInfo = this.MessageJsonData[e].info
+        }
     },
 })
 const slideOut = () =>{
@@ -21,4 +38,7 @@ const slideIn = () =>{
     $('.MessageBox').animate({
         right: '-200px'
     }, 500)
+}
+function SwitchInfos(e){
+    SwitchInfo(e)
 }
